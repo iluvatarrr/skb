b4916fbe2cb486a74a27448219be6f62ae82987d
iluvatar <kds02122004@mail.ru>
2025-02-25 00:15:19 +0500

    init commit

---
+++ src/test/java/com/example/skb_spring/SkbSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skb_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkbSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
