0a40e7da08afe998f1d5f3ffe6630c48134107ad
iluvatar <kds02122004@mail.ru>
2025-03-29 15:21:45 +0500

    delete handler from controller

--- src/main/java/com/example/skb_spring/controller/ValidTaskController.java
+++ src/main/java/com/example/skb_spring/controller/ValidTaskController.java
@@ -4,15 +4,9 @@ import com.example.skb_spring.dto.WorkerDto;
 import com.example.skb_spring.service.WorkerService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @AllArgsConstructor
@@ -29,19 +23,6 @@ public class ValidTaskController {
     @PostMapping("/validation")
     public void addWorker(@RequestBody @Valid WorkerDto workerDto) {
         workerService.add(workerDto);
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
 }
